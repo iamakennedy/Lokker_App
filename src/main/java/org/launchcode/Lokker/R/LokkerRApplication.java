7ace0788f86826c70e8e8eb0b2704e83d36e76ad
Alyssa Kennedy <kennedy.alyssa@gmail.com>
2019-11-25 15:38:00 -0500

    initial commit

---
+++ src/main/java/org/launchcode/Lokker/R/LokkerRApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Lokker.R;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokkerRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokkerRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Lokker/R/LokkerRApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Lokker.R;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokkerRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
